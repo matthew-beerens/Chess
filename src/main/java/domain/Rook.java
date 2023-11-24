ffb12f8868b9bdfbaf98e43929816f4978dca7d7
matthew-beerens <matthew.beerens@gmail.com>
2023-11-24 23:26:18 +1100

    implement rook get moves as well as implement tests for rook getmoves.

--- src/main/java/domain/Rook.java
+++ src/main/java/domain/Rook.java
@@ -1,10 +1,11 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Rook extends Piece{
     public Rook() {
-        super(PieceType.ROOK);
+        this(PieceColor.NULL);
     }
 
     public Rook(PieceColor color) {
@@ -13,7 +14,82 @@ public class Rook extends Piece{
 
     @Override
     public List<BoardSquare> getMoves(ChessBoard chessboard, SquarePosition position) {
-        return null;
+        ArrayList<BoardSquare> moves = new ArrayList<>();
+        PieceColor color = this.getColor().equals(PieceColor.WHITE) ? PieceColor.BLACK : PieceColor.WHITE;
+
+        for (int i = position.getY() - 1; i >= 0; i--) { // moves to left
+            BoardSquare bs = chessboard.getSquare(position.getX(), i);
+
+            if (bs == null) {
+                break;
+            }
+
+            if (bs.getPiece().getColor().equals(this.getColor())) {
+                break;
+            }
+
+            moves.add(bs);
+
+            if(bs.getPiece().getColor().equals(color)) {
+                break;
+            }
+        }
+
+        for (int i = position.getY() + 1; i < 8 ; i++) { // moves to the right
+            BoardSquare bs = chessboard.getSquare(position.getX(), i);
+
+            if (bs == null) {
+                break;
+            }
+
+            if (bs.getPiece().getColor().equals(this.getColor())) {
+                break;
+            }
+
+            moves.add(bs);
+
+            if(bs.getPiece().getColor().equals(color)) {
+                break;
+            }
+        }
+
+        for (int i = position.getX() - 1; i >= 0; i--) { // moves above
+            BoardSquare bs = chessboard.getSquare(i, position.getY());
+
+            if (bs == null) {
+                break;
+            }
+
+            if (bs.getPiece().getColor().equals(this.getColor())) {
+                break;
+            }
+
+            moves.add(bs);
+
+            if(bs.getPiece().getColor().equals(color)) {
+                break;
+            }
+        }
+
+        for (int i = position.getX() + 1; i < 8; i++) { // moves below
+            BoardSquare bs = chessboard.getSquare(i, position.getY());
+
+            if (bs == null) {
+                break;
+            }
+
+            if (bs.getPiece().getColor().equals(this.getColor())) {
+                break;
+            }
+
+            moves.add(bs);
+
+            if(bs.getPiece().getColor().equals(color)) {
+                break;
+            }
+        }
+
+        return moves;
     }
 
 }
--- src/test/java/RookTest.java
+++ src/test/java/RookTest.java
@@ -1,10 +1,11 @@
-import domain.Bishop;
-import domain.PieceColor;
-import domain.PieceType;
-import domain.Rook;
+import domain.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RookTest {
     private Rook rook = new Rook();
@@ -35,5 +36,77 @@ public class RookTest {
     void pieceIsNotAColor() {
         Rook nullPiece = new Rook();
         assertEquals(nullPiece.getColor(), PieceColor.NULL);
+    }
+
+    @Test
+    void getMoves() {
+        ChessBoard cb = new ChessBoard();
+        cb.initialize(PieceColor.WHITE, PieceColor.BLACK);
+        cb.placePiece(new Rook(PieceColor.WHITE), 3, 3);
+        SquarePosition sp = cb.getSquare(3, 3).getPosition();
+        List<BoardSquare> moves = cb.getPiece(3, 3).getMoves(cb, sp);
+        List<BoardSquare> expectedAvailableMoves = new ArrayList<>();
+
+        for (int i = sp.getY() - 1; i >= 0; i--) { // moves to left
+            if (cb.getSquare(sp.getX(), i).getPiece().getColor().equals(PieceColor.WHITE)) {
+                break;
+            }
+
+            BoardSquare bs = new BoardSquare();
+            bs.getPosition().setPosition(sp.getX(), i);
+            expectedAvailableMoves.add(bs);
+
+            if(cb.getSquare(sp.getX(), i).getPiece().getColor().equals(PieceColor.BLACK)) {
+                break;
+            }
+        }
+
+        for (int i = sp.getY() + 1; i < 8 ; i++) { // moves to the right
+            if (cb.getSquare(sp.getX(), i).getPiece().getColor().equals(PieceColor.WHITE)) {
+                break;
+            }
+
+            BoardSquare bs = new BoardSquare();
+            bs.getPosition().setPosition(sp.getX(), i);
+            expectedAvailableMoves.add(bs);
+
+            if(cb.getSquare(sp.getX(), i).getPiece().getColor().equals(PieceColor.BLACK)) {
+                break;
+            }
+        }
+
+        for (int i = sp.getX() - 1; i >= 0; i--) { // moves above
+            if (cb.getSquare(i, sp.getY()).getPiece().getColor().equals(PieceColor.WHITE)) {
+                break;
+            }
+
+            BoardSquare bs = new BoardSquare();
+            bs.getPosition().setPosition(i, sp.getY());
+            expectedAvailableMoves.add(bs);
+
+            if(cb.getSquare(i, sp.getY()).getPiece().getColor().equals(PieceColor.BLACK)) {
+                break;
+            }
+        }
+
+        for (int i = sp.getX() + 1; i < 8; i++) { // moves below
+            if (cb.getSquare(i, sp.getY()).getPiece().getColor().equals(PieceColor.WHITE)) {
+                break;
+            }
+
+            BoardSquare bs = new BoardSquare();
+            bs.getPosition().setPosition(i, sp.getY());
+            expectedAvailableMoves.add(bs);
+
+            if(cb.getSquare(i, sp.getY()).getPiece().getColor().equals(PieceColor.BLACK)) {
+                break;
+            }
+        }
+
+        for (BoardSquare bs :
+                expectedAvailableMoves) {
+            assertTrue(moves.contains(bs));
+        }
+
     }
 }
