02a2fbbd46ffc0a746a21f4c81b898748efc34c8
matthew-beerens <matthew.beerens@gmail.com>
2023-11-30 23:03:46 +1100

    add tests for bishop getMoves and implement bishop getMoves()

--- src/main/java/domain/Bishop.java
+++ src/main/java/domain/Bishop.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bishop extends Piece{
@@ -12,7 +13,49 @@ public class Bishop extends Piece{
 
     @Override
     public List<BoardSquare> getMoves(ChessBoard chessboard, SquarePosition position) {
-        return null;
+        ArrayList<BoardSquare> moves = new ArrayList<>();
+
+        // left top diagonal
+        int y = position.getY() - 1;
+        for (int x = position.getX() - 1; x < 8; x--) {
+            BoardSquare bs = chessboard.getSquare(x, y);
+            if(!this.addMove(moves, bs, this.getOpposingColor())) {
+                break;
+            }
+            y--;
+        }
+
+        // left bottom diagonal
+        y = position.getY() - 1;
+        for (int x = position.getX() + 1; x < 8; x++) {
+            BoardSquare bs = chessboard.getSquare(x, y);
+            if(!this.addMove(moves, bs, this.getOpposingColor())) {
+                break;
+            }
+            y--;
+        }
+
+        // right top diagonal
+        y = position.getY() + 1;
+        for (int x = position.getX() - 1; x < 8; x--) {
+            BoardSquare bs = chessboard.getSquare(x, y);
+            if(!this.addMove(moves, bs, this.getOpposingColor())) {
+                break;
+            }
+            y++;
+        }
+
+        // right bottom diagonal
+        y = position.getY() + 1;
+        for (int x = position.getX() + 1; x < 8; x++) {
+            BoardSquare bs = chessboard.getSquare(x, y);
+            if(!this.addMove(moves, bs, this.getOpposingColor())) {
+                break;
+            }
+            y++;
+        }
+
+        return moves;
     }
 
 
--- src/test/java/BishopTest.java
+++ src/test/java/BishopTest.java
@@ -1,9 +1,11 @@
-import domain.Bishop;
-import domain.PieceColor;
-import domain.PieceType;
+import domain.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BishopTest {
     private Bishop bishop = new Bishop();
@@ -34,6 +36,48 @@ public class BishopTest {
     void pieceIsNotAColor() {
         Bishop nullPiece = new Bishop();
         assertEquals(nullPiece.getColor(), PieceColor.NULL);
+    }
+
+    @Test
+    void getMoves() {
+        ChessBoard cb = new ChessBoard();
+        cb.initialize(PieceColor.WHITE, PieceColor.BLACK);
+        cb.placePiece(new Bishop(PieceColor.WHITE), 3, 5);
+        Piece bishop = (Bishop) cb.getPiece(3, 5);
+        BoardSquare square = cb.getSquare(3, 5);
+
+        List<BoardSquare> moves = bishop.getMoves(cb, square.getPosition());
+        ArrayList<BoardSquare> expectedMoves = new ArrayList<>();
+
+        BoardSquare bs0 = new BoardSquare();
+        bs0.getPosition().setPosition(2, 4);
+        expectedMoves.add(bs0);
+        BoardSquare bs1 = new BoardSquare();
+        bs1.getPosition().setPosition(1, 3);
+        expectedMoves.add(bs1);
+        BoardSquare bs2 = new BoardSquare();
+        bs2.getPosition().setPosition(2, 6);
+        expectedMoves.add(bs2);
+        BoardSquare bs3 = new BoardSquare();
+        bs3.getPosition().setPosition(1, 7);
+        expectedMoves.add(bs3);
+        BoardSquare bs4 = new BoardSquare();
+        bs4.getPosition().setPosition(4, 4);
+        expectedMoves.add(bs4);
+        BoardSquare bs5 = new BoardSquare();
+        bs5.getPosition().setPosition(5, 3);
+        expectedMoves.add(bs5);
+        BoardSquare bs6 = new BoardSquare();
+        bs6.getPosition().setPosition(4, 6);
+        expectedMoves.add(bs6);
+        BoardSquare bs7 = new BoardSquare();
+        bs7.getPosition().setPosition(5, 7);
+        expectedMoves.add(bs7);
+
+        for (BoardSquare m :
+                expectedMoves) {
+            assertTrue(moves.contains(m));
+        }
     }
 
 }
