a95bf8256a41dd7051376280d4ad9be2a6ed9110
matthew-beerens <matthew.beerens@gmail.com>
2023-11-30 23:02:44 +1100

    add getOpposingColor() and addMove() to Piece class

--- src/main/java/domain/Piece.java
+++ src/main/java/domain/Piece.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -39,6 +40,10 @@ public abstract class Piece {
         return this.type.toString();
     }
 
+    public PieceColor getOpposingColor() {
+        return this.getColor().equals(PieceColor.WHITE) ? PieceColor.BLACK : PieceColor.WHITE;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -52,4 +57,23 @@ public abstract class Piece {
     }
 
     public abstract List<BoardSquare> getMoves(ChessBoard chessboard, SquarePosition position);
+
+    public boolean addMove(ArrayList<BoardSquare> moves, BoardSquare bs, PieceColor color) {
+
+        if (bs == null) {
+            return false;
+        }
+
+        if (bs.getPiece().getColor().equals(this.getColor())) {
+            return false;
+        }
+
+        moves.add(bs);
+
+        if(bs.getPiece().getColor().equals(color)) {
+            return false;
+        }
+
+        return true;
+    }
 }
