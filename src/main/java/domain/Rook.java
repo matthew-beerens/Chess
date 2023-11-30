7c2bd29f5ea893d32af1dd2e4389d87b6abcdd15
matthew-beerens <matthew.beerens@gmail.com>
2023-11-30 23:03:16 +1100

    refactor rook to use new methods in Piece class - getOpposingColor and addMove

--- src/main/java/domain/Rook.java
+++ src/main/java/domain/Rook.java
@@ -12,54 +12,34 @@ public class Rook extends Piece{
         super(PieceType.ROOK, color);
     }
 
-    public boolean addBoardSquare(ArrayList<BoardSquare> moves, BoardSquare bs, PieceColor color) {
-
-        if (bs == null) {
-            return false;
-        }
-
-        if (bs.getPiece().getColor().equals(this.getColor())) {
-            return false;
-        }
-
-        moves.add(bs);
-
-        if(bs.getPiece().getColor().equals(color)) {
-            return false;
-        }
-
-        return true;
-    }
-
     @Override
     public List<BoardSquare> getMoves(ChessBoard chessboard, SquarePosition position) {
         ArrayList<BoardSquare> moves = new ArrayList<>();
-        PieceColor color = this.getColor().equals(PieceColor.WHITE) ? PieceColor.BLACK : PieceColor.WHITE;
 
         for (int i = position.getY() - 1; i >= 0; i--) { // moves to left
             BoardSquare bs = chessboard.getSquare(position.getX(), i);
-            if (!this.addBoardSquare(moves, bs, color)) {
+            if (!this.addMove(moves, bs, this.getOpposingColor())) {
                 break;
             }
         }
 
         for (int i = position.getY() + 1; i < 8 ; i++) { // moves to the right
             BoardSquare bs = chessboard.getSquare(position.getX(), i);
-            if (!this.addBoardSquare(moves, bs, color)) {
+            if (!this.addMove(moves, bs, this.getOpposingColor())) {
                 break;
             }
         }
 
         for (int i = position.getX() - 1; i >= 0; i--) { // moves above
             BoardSquare bs = chessboard.getSquare(i, position.getY());
-            if (!this.addBoardSquare(moves, bs, color)) {
+            if (!this.addMove(moves, bs, this.getOpposingColor())) {
                 break;
             }
         }
 
         for (int i = position.getX() + 1; i < 8; i++) { // moves below
             BoardSquare bs = chessboard.getSquare(i, position.getY());
-            if (!this.addBoardSquare(moves, bs, color)) {
+            if (!this.addMove(moves, bs, this.getOpposingColor())) {
                 break;
             }
         }
