10b46ccb54570a9899ea3748558c09f5f898cd26
matthew-beerens <matthew.beerens@gmail.com>
2023-12-01 12:10:07 +1100

    add tests got king getMoves() and implement king getMoves()

--- src/main/java/domain/King.java
+++ src/main/java/domain/King.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class King extends Piece{
@@ -13,7 +14,20 @@ public class King extends Piece{
 
     @Override
     public List<BoardSquare> getMoves(ChessBoard chessboard, SquarePosition position) {
-        return null;
+        ArrayList<BoardSquare> moves = new ArrayList<>();
+        // top row
+        this.addMove(moves, chessboard.getSquare(position.getX() - 1, position.getY() - 1), this.getOpposingColor());
+        this.addMove(moves, chessboard.getSquare(position.getX() - 1, position.getY()), this.getOpposingColor());
+        this.addMove(moves, chessboard.getSquare(position.getX() - 1, position.getY() + 1), this.getOpposingColor());
+        // middle row
+        this.addMove(moves, chessboard.getSquare(position.getX(), position.getY() - 1), this.getOpposingColor());
+        this.addMove(moves, chessboard.getSquare(position.getX(), position.getY() + 1), this.getOpposingColor());
+        // bottom row
+        this.addMove(moves, chessboard.getSquare(position.getX() + 1, position.getY() - 1), this.getOpposingColor());
+        this.addMove(moves, chessboard.getSquare(position.getX() + 1, position.getY()), this.getOpposingColor());
+        this.addMove(moves, chessboard.getSquare(position.getX() + 1, position.getY() + 1), this.getOpposingColor());
+
+        return moves;
     }
 
 
--- src/test/java/KingTest.java
+++ src/test/java/KingTest.java
@@ -1,10 +1,11 @@
-import domain.Bishop;
-import domain.King;
-import domain.PieceColor;
-import domain.PieceType;
+import domain.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class KingTest {
     private King king = new King();
@@ -34,5 +35,53 @@ public class KingTest {
     void pieceIsNotAColor() {
         King nullPiece = new King();
         assertEquals(nullPiece.getColor(), PieceColor.NULL);
+    }
+
+    @Test
+    void kingGetMoves() {
+        ChessBoard cb = new ChessBoard();
+        cb.initialize(PieceColor.WHITE, PieceColor.BLACK);
+        System.out.println(cb);
+        List<BoardSquare> moves = cb.getPiece(7,4).getMoves(cb, cb.getSquare(7, 4).getPosition());
+        ArrayList<BoardSquare> expectedMoves = new ArrayList<>();
+        assertTrue(moves.isEmpty());
+        cb.placePiece(new King(PieceColor.WHITE), 2, 4);
+        
+        BoardSquare bs0 = new BoardSquare();
+        bs0.getPosition().setPosition(1, 3);
+        expectedMoves.add(bs0);
+
+        BoardSquare bs1 = new BoardSquare();
+        bs1.getPosition().setPosition(1, 4);
+        expectedMoves.add(bs1);
+
+        BoardSquare bs2 = new BoardSquare();
+        bs2.getPosition().setPosition(1, 5);
+        expectedMoves.add(bs2);
+
+        BoardSquare bs3 = new BoardSquare();
+        bs3.getPosition().setPosition(2, 3);
+        expectedMoves.add(bs3);
+        
+        BoardSquare bs4 = new BoardSquare();
+        bs4.getPosition().setPosition(2, 5);
+        expectedMoves.add(bs4);
+
+        BoardSquare bs5 = new BoardSquare();
+        bs5.getPosition().setPosition(3, 3);
+        expectedMoves.add(bs5);
+
+        BoardSquare bs6 = new BoardSquare();
+        bs6.getPosition().setPosition(3, 4);
+        expectedMoves.add(bs6);
+
+        BoardSquare bs7 = new BoardSquare();
+        bs7.getPosition().setPosition(3, 5);
+        expectedMoves.add(bs7);
+
+        for (BoardSquare m :
+                expectedMoves) {
+            moves.contains(m);
+        }
     }
 }
