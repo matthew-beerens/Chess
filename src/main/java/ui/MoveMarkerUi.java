af8c3d40ab3da269e2d9c06e6e4ee4760448ab73
matthew-beerens <matthew.beerens@gmail.com>
2023-12-05 12:45:33 +1100

    implement class for marking available moves on chessboard

---
+++ src/main/java/ui/MoveMarkerUi.java
@@ -0,0 +1,10 @@
+package ui;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class MoveMarkerUi extends Rectangle {
+    public MoveMarkerUi() {
+        super(90, 90, Color.rgb(0, 0,255, 0.3));
+    }
+}
