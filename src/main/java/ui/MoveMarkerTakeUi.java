f2f04ee929253fbc56b37b56d81de150809e0637
matthew-beerens <matthew.beerens@gmail.com>
2023-12-05 23:22:57 +1100

    change opacity

--- src/main/java/ui/MoveMarkerTakeUi.java
+++ src/main/java/ui/MoveMarkerTakeUi.java
@@ -7,7 +7,7 @@ public class MoveMarkerTakeUi extends Circle {
     public MoveMarkerTakeUi() {
         super(39);
         this.setFill(Color.rgb(0,0,0,0));
-        this.setStroke(Color.rgb(50,50,50,0.5));
+        this.setStroke(Color.rgb(50,50,50,0.3));
         this.setStrokeWidth(10);
     }
 }
