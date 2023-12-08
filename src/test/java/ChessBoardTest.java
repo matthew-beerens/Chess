f7713ff3fe95fecea1c60ae34c868c98cb633cfb
matthew-beerens <matthew.beerens@gmail.com>
2023-12-08 12:16:49 +1100

    add enpassantable to chessboard tests

--- src/test/java/ChessBoardTest.java
+++ src/test/java/ChessBoardTest.java
@@ -130,4 +130,14 @@ public class ChessBoardTest {
         cb.movePiece(cb.getSquare(7, 4), cb.getSquare(7, 7));
         assertEquals(cb2.toString(), cb.toString());
     }
+
+    @Test
+    void enpassantable() {
+        ChessBoard cb = new ChessBoard();
+        cb.initialize(PieceColor.WHITE, PieceColor.BLACK);
+        BoardSquare dest = cb.getPiece(6, 4).getMoves(cb, cb.getSquare(6, 4).getPosition()).get(1);
+        cb.movePiece(cb.getSquare(6, 4), dest);
+        Pawn pawn = (Pawn) dest.getPiece();
+        assertEquals(true, pawn.isEnpassantable());
+    }
 }
